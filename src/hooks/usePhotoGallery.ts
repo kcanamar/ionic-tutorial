23c668f57f755aa59331978110bfc1cebe4af8f5
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-03-16 13:33:18 -0700

    load saved photos froim the filesystem

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -11,9 +11,28 @@ import {
 import { Filesystem, Directory } from "@capacitor/filesystem" 
 import { Preferences } from "@capacitor/preferences"
 import { Capacitor } from "@capacitor/core";
-
+const PHOTO_STORAGE = 'photos'
 export function usePhotoGallery() {
     const [photos, setPhotos] = useState<UserPhoto[]>([])
+
+    // retrieve data when the hook loads
+    useEffect(() => {
+        const loadSaved = async () => {
+            const { value } = await Preferences.get({key: PHOTO_STORAGE})
+            const photosInPreferences = (value ? JSON.parse(value) : []) as UserPhoto[]
+
+            for (let photo of photosInPreferences) {
+                const file = await Filesystem.readFile({
+                    path: photo.filepath,
+                    directory: Directory.Data,
+                })
+                // Web platform only: Load the photo as base64 data
+                photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
+            }
+            setPhotos(photosInPreferences)
+        }
+        loadSaved()
+    },[])
 
     const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
         const base64Data = await base64FromPath(photo.webPath!);
@@ -46,6 +65,7 @@ export function usePhotoGallery() {
             ...photos,
         ];
         setPhotos(newPhotos)
+        Preferences.set({ key: PHOTO_STORAGE, value:JSON.stringify(newPhotos)})
     }
 
     return {
