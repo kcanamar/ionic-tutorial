d2a3f9af2a2d01b53a8c4b527d261266dce28646
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-03-16 13:22:41 -0700

    save file to filesystem

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -13,7 +13,23 @@ import { Preferences } from "@capacitor/preferences"
 import { Capacitor } from "@capacitor/core";
 
 export function usePhotoGallery() {
-  const [photos, setPhotos] = useState<UserPhoto[]>([])
+    const [photos, setPhotos] = useState<UserPhoto[]>([])
+
+    const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
+        const base64Data = await base64FromPath(photo.webPath!);
+        const savedFile = await Filesystem.writeFile({
+          path: fileName,
+          data: base64Data,
+          directory: Directory.Data,
+        });
+    
+        // Use webPath to display the new image instead of base64 since it's
+        // already loaded into memory
+        return {
+          filepath: fileName,
+          webviewPath: photo.webPath,
+        };
+      };
 
     const takePhoto = async () => {
         // call the camera to get the photo
@@ -24,11 +40,9 @@ export function usePhotoGallery() {
         })
         // display the photos
         const fileName = new Date().getTime() + ".jpeg";
+        const savedFileImage = await savePicture(photo, fileName);
         const newPhotos = [
-            {
-                filepath: fileName,
-                webviewPath: photo.webPath,
-            },
+            savedFileImage,
             ...photos,
         ];
         setPhotos(newPhotos)
@@ -38,6 +52,24 @@ export function usePhotoGallery() {
         photos,
         takePhoto,
     }
+}
+
+export async function base64FromPath(path: string): Promise<string> {
+    const response = await fetch(path);
+    const blob = await response.blob();
+
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onerror = reject;
+        reader.onload = () => {
+            if (typeof reader.result === 'string') {
+                resolve(reader.result);
+            } else {
+                reject('method did not return a string');
+            }
+        }
+        reader.readAsDataURL(blob);
+    })
 }
 
 export interface UserPhoto {
